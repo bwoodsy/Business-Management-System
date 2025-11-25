f7369237caebb2978c997a9d6acb4e13cc4c5754
Braden Woods <bwoodsy12@gmail.com>
2025-11-25 11:33:04 -0700

    feat: init category services in program.cs

--- backend/BusinessManagementSystem.Api/Program.cs
+++ backend/BusinessManagementSystem.Api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
