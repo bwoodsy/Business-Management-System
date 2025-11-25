62c7fb347e004d6f956ba717ae462d9c2e0e4431
Braden Woods <bwoodsy12@gmail.com>
2025-11-25 11:30:32 -0700

    feat: added input validation for Category

--- backend/BusinessManagementSystem.Api/Models/Category.cs
+++ backend/BusinessManagementSystem.Api/Models/Category.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessManagementSystem.Api.Models;
 
 public class Category
 {
     public int Id { get; set ;}
+
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
 }
