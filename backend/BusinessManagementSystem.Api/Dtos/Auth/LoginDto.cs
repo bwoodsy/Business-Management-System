e2248ebb84bc728fbfad809c534514d58515d73c
Braden Woods <bwoodsy12@gmail.com>
2026-01-01 20:50:08 -0700

    feat: auth dtos

---
+++ backend/BusinessManagementSystem.Api/Dtos/Auth/LoginDto.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessManagementSystem.Api.Dtos;
+
+public class LoginDto
+{
+    [Required] public string Username { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
+}
