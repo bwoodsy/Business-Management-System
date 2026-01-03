a18ac78672799beda77237779a6e0f653e1658fd
Braden Woods <bwoodsy12@gmail.com>
2026-01-03 11:01:15 -0700

    feat: ui shell

--- frontend/bms-web/src/app/layout/shell/shell.ts
+++ frontend/bms-web/src/app/layout/shell/shell.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './shell.html',
-  styleUrl: './shell.css',
+  styleUrls: ['./shell.css'],
 })
 export class Shell {
+  constructor(public auth: AuthService) {}
 
+  logout() {
+    this.auth.logout();
+  }
 }
